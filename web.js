dadacebe803677527187873beafc96e241b74d05
Pedro Guerreiro <pguerr@gmail.com>
2013-07-22 19:19:44 +0000

    Added a missing }

--- web.js
+++ web.js
@@ -5,6 +5,7 @@ app.use(express.logger());
 app.get('/', function(request, response) {
   response.send('Hello World again!');
   response.send('Very much hello!');
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
