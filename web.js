395f6ae536a394279fd95f51d54f44014ad34bbe
Pedro Guerreiro <pguerr@gmail.com>
2013-07-23 10:24:16 +0000

    Homework edits.

--- web.js
+++ web.js
@@ -4,8 +4,16 @@ var app = express();
 
 app.use(express.logger());
 
+var fs = requires('fs');
+
+var buffer = new Buffer(32);
+buffer = fs.readFile("index.html");
+
+var message = buffer.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World again and again!');
+//  response.send('Hello World again and again!');  
+  response.send(message);
 });
 
 var port = process.env.PORT || 5000;
